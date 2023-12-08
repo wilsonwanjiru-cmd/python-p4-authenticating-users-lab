60ac033259cf6a7c0d739a15762203eca7bf20f3
Wilson Wanjiru <wilsonmuita41@gmail.com>
2023-12-08 20:32:21 +0300

    Authenticating Users Lab

--- server/app.py
+++ server/app.py
@@ -47,7 +47,44 @@ class ShowArticle(Resource):
             return make_response(article_json, 200)
 
         return {'message': 'Maximum pageview limit reached'}, 401
+    
+class LogIn(Resource):
 
+    def post(self):
+        username = request.get_json()['username']
+        user = User.query.filter(User.username == username).first()
+        session['user_id'] = user.id
+
+        response = make_response(
+            jsonify(user.to_dict()),
+            200
+        )
+        return response
+
+class Logout(Resource):
+
+    def delete(self):
+        session['user_id'] = None
+
+        response = make_response(
+            jsonify({'message': '204: No Content'}),
+            204
+        )
+        return response
+
+class CheckSession(Resource):
+
+    def get(self):
+        user= User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return make_response(jsonify(user.to_dict()),200)
+        else:
+            return make_response({},401)
+        
+
+api.add_resource(LogIn, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
